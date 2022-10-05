0753d27bd3c76a96b594a9e42f57ca327c7186bb
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-05 21:49:44 +0600

    get all tour with filtering

--- Controllers/Tour.controler.js
+++ Controllers/Tour.controler.js
@@ -1,9 +1,37 @@
-const { postATour } = require("../Services/Tour.services");
+const { postATour,getAllTours } = require("../Services/Tour.services");
 
 //Get all products
 exports.getAllTours = async(req, res,next) => {
 try {
-    
+    let tourObj = {...req.query};
+    const excludeFields = ['sort','limit','page'];
+    excludeFields.forEach(field=>delete tourObj[field]);
+    let queryStr = JSON.stringify(tourObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match=>`$${match}`);
+    tourObj = JSON.parse(queryStr);
+    const queries = {}
+    //sort
+    if(req.query.sort){
+      const sortBy = req.query.sort.split(',').join(' ');
+      queries.sortBy = sortBy;
+    }
+    //filtering
+    if(req.query.fields){
+      const fields = req.query.fields.split(',').join(' ');
+      queries.fields = fields;
+    }
+    //pagination
+      if(req.query.page){
+      const {page =1,limit = 10} = req.query;
+      const skip = (page-1)*parseInt(limit);
+      queries.skip = skip;
+      queries.limit = parseInt(limit);
+      }
+      const tour = await getAllTours(tourObj,queries);
+      res.status(200).json({
+        status: "success",
+        data: tour
+      })
 } catch (error) {
     next(error)
 }
--- Services/Tour.services.js
+++ Services/Tour.services.js
@@ -5,3 +5,12 @@ exports.postATour = async(tour) => {
     const data = await newTour.save();
     return data;
 }
+exports.getAllTours = async(tourObj,queries) => {
+const tour = await Tour.find(tourObj)
+ .skip(queries.skip)
+ .limit(queries.limit)
+ .select(queries.fields)
+ .sort(queries.sortBy);
+ const toatalTours = await Tour.countDocuments(tourObj);
+return {tour,toatalTours};
+}
