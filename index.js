180fbaa84993f14c68dfcd47ac22d3ba3a623ead
saymanrabbi <mdrabbi.asm@gmail.com>
2022-10-06 13:34:10 +0600

    server changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const tour = require('./Routes/Tour.route');
 const { ErrorHandeler } = require('./Middleware/Error.handeler');
+const dbConnection = require('./server');
 
 // Middleware
 app.use(cors());
@@ -12,6 +13,11 @@ app.use('/api/v1/tour', tour);
 
 app.get("/",(req,res)=>{
     res.send("Route Is Working!!")
+})
+dbConnection()
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`.yellow.bold);
 })
 app.all("*", (req, res) => {
     res.send("404 not found")
